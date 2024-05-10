6b6524c385d7a61313cb368dd93ecc591fb44f68
Teo1112 <t.puiu@yahoo.com>
2024-05-10 21:20:41 +0300

    primul exercitiu din tema

---
+++ ex1/index.js
@@ -0,0 +1,10 @@
+function divizibilCuZece(nr){
+    if(nr % 10 === 0){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+console.log(divizibilCuZece(100));
+console.log(divizibilCuZece(23));
